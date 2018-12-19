153c14957ee1cb2efa51765a7b4fc70529490175
Peng Cheng <chengandpeng@gmail.com>
2018-12-19 22:59:53 +0800

    lodash linear regression

--- regressions/index.js
+++ regressions/index.js
@@ -1,6 +1,7 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
     shuffle: true,
@@ -8,3 +9,12 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
     dataColumns: ['horsepower'],
     labelColumns: ['mpg']    
 });
+
+const regression = new LinearRegression(features, labels, {
+    learningRate: 0.00001,
+    iterations: 1000
+});
+
+regression.train();
+
+console.log('Update M is:', regression.m, 'Updated B is:', regression.b);
